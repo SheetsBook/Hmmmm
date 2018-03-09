cae0d41a4313b3b23dec8d1db762e978e239015f
X-49 <35293284+X-49@users.noreply.github.com>
2018-03-10 01:29:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const vm = require("vm");
 const codeContext =  {};
 vm.createContext(codeContext);
 const client = new Discord.Client();
-const prefix = "e.";
+const prefix = "ev.";
 
 client.on("ready", () => {
-  console.log("Bot is online!");
-  client.user.setActivity(`e.help in ${client.guilds.size} servers!`);
+  console.log("Я готов!");
+  client.user.setPresence({ game: { name: `ev. для эмуляции`, type: 1 } }).catch();
 });
 
 client.on("message", async(message) => {
@@ -42,16 +42,10 @@ client.on("message", async(message) => {
    }
 	} else if(command === 'help') {
 		message.channel.send(`== Help ==
-All commands:
-invite :: Invite me to your server
-eval :: evalute js code
-help :: shows this page
-more commands coming soon suggest an idea by DMing Free TNT#5796
-Disclaimer:
-This bot is an eval which runs ANY javascript code which is like users can even wreck your server mass DM or anything so use it at your own risk i'm not responsible if someone did a malicious code that destroys your server.
-for safety the invite link does not add any permissions to the bot it's up to you to give it permissions if you want but as i said at your own risk i'm not responsible for bad things happening in your server. KEEP IT NO PERMISSIONS FOR SAFETY, and those hackers reading this don't think of hacking me, the token is safe, and i'm using heroku which doesn't give access to my files and it's free host thing idc much.`, {code: "asciidoc"});
+Тут могла быть ваша реклама.
+ev. для эмуляции``, {code: "asciidoc"});
 	} else if(command === "invite") {
-		message.reply("By inviting me to your server you agree that i'm not responsible for any damage on your server for safety the URL doesn't grant the bot any permissions\nhttps://discordapp.com/api/oauth2/authorize?client_id=411952600586649602&permissions=0&scope=bot")
+		message.reply("Тут могла быть ваша реклама :D ")
 	} else if(command === "beval") {
     try {
   let evaled = vm.runInContext(args.join(" "), codeContext);
