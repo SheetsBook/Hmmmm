2af88b1c4c925b4883fe3141e121cd08187e265b
X-495 <35427055+X-495@users.noreply.github.com>
2018-03-14 20:42:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ client.on("message", async(message) => {
     output = output.replace(filter, "[TOKEN]");
     output = clean(output);
     if (output.length < 1950) {
-      message.channel.send(`\`\`\`js\n${output}\n\`\`\``);
+      message.author.send(`\`\`\`js\n${output}\n\`\`\``);
     } else {
     	  message.author.send(`${output}`, {split:"\n", code:"js"});
     }
