09d5fd6cd578ef852cdf8a56407eaa31808a1771
X-495 <35427055+X-495@users.noreply.github.com>
2018-03-27 11:18:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const codeContext =  {};
 vm.createContext(codeContext);
 const client = new Discord.Client();
 const prefix = "x!";
-const rule = {own: "421373056158662656"}
+const rule = {own: "421373056158662656", admin: "421244086557605888"}
 const creators = ['207821802431315968']
 const emojis = {mila:'418748638081318912'}
 
 
 client.on("ready", () => {
   console.log("врум врум");
-  client.user.setActivity(`x!help`);
+  client.user.setActivity(`x!eval`);
 });
 
 client.on('message', message => {
@@ -67,30 +67,29 @@ client.fetchWebhook('427002003005243393', 'SvggGN-ntZGC_2T-0eJSTCUIk2tcKrCwhZ6l-
 		message.channel.send(`**Репорт пользователя ${message.author} успешно принят**`);
 	    message.delete();
 
-        } else if(command === "help" || command === "h") {
-		  const embed = new Discord.RichEmbed()
-	  .setTitle('Команды бота.')
-	  .setColor("#42f4aa")
-	  .setDescription("**x!eval** эмуляция js кода (bot owner) \n**x!report** жалоба на пользователя \n**x!say** сообщение от бота. \n**бот пиши** начну писать в чат где вы меня вызвали. \n**бот не пиши** перестану писать в чат где вы меня вызвали")
-	.addField('Eval', 'Позволяет владельцу бота запускать произвольный код из аккаунта бота. \nПредупреждение: Эта команда может быть чрезвычайно опасной. \nЕсли вы не знаете, что она делает, вы можете вызвать ужасные последствия | проблемы на вашем сервере или с этим ботом. \nНИКОГДА не запускайте эту команду, если вы не полностью уверены в том, за что отвечает эта команда.')
-	.addField('report', 'Если бот находится на сторонем сервере в не рамках сервера │Ø Ĥ Є Я │ прошу не использовать данную команду, \nтак как сервера разные а канал репортов всего 1. \nПозволяет отправлять репорты.')
-	.addField('say', 'Команда доступная для всех, позволяет писать от имени бота')
-	  .setFooter("Префикс бота - x! ; Помощь - x!help")
-	  .setTimestamp();
-	  message.channel.send({embed});
-	} else if(command === "say") {
-        const sayMessage = args.join(" ");
-        message.delete().catch(O_o => {});
-        message.channel.send(sayMessage);
-	} else if(command === "rs") {
-		if(!message.member.roles.some(r=>[rule.own].includes(r.id)) && !creators.includes(message.author.id))
-  			return message.reply("Извините, ебобобам слово не давали!");
-		if (message.channel.id = undefined) return message.author.send('ид канала?');
+        } else if(command === "rs" || command === "remote") {
+		if(!message.member.roles.some(r=>[rule.own, rule.admin].includes(r.id)) && !creators.includes(message.author.id))
+  			return message.reply("Извините, прав нет!");
+		if (message.channel.id = undefined) return message.author.send('А канал??');
 		let new_args = args;
 		const chat = new_args.shift();
 	 	const sayMessage = new_args.join(" ");
 	 	console.log(chat);
-	    message.guild.channels.get(chat).send(sayMessage).catch(()=>{message.reply('ты ебобо?');});
+	    message.guild.channels.get(chat).send(sayMessage).catch(()=>{message.reply('А сообщение?');});
 	    message.delete().catch(O_o=>{}); 
+	} else if(command === "say") {
+        const sayMessage = args.join(" ");
+        message.delete().catch(O_o => {});
+        message.channel.send(sayMessage);
+	} else if(command === "beval") {
+    try {
+  let evaled = vm.runInContext(args.join(" "), codeContext);
+  message.channel.send(evaled, {code:"js",split:"\n"});
+} catch(e) {
+ message.channel.send(e, {code:"js",split:"\n"});
+}
+}
+	
+});
 
 client.login(process.env.TOKEN);
