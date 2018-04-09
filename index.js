8d2edf995b97e8786059d34c8022b3209cf71399
Артем Кучуров <35427055+X-495@users.noreply.github.com>
2018-04-09 14:42:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const client = new Discord.Client();
 const prefix = "x!";
 const rule = {own: "421373056158662656", admin: "421244086557605888"};
 const creators = ['207821802431315968'];
-const emojis = {nya:'432192390569132032', protiv:'419121914959626240', neznayu:'419121999277719562', za:'419122029854457866'};
+const emojis = {:'432192390569132032', loading..:'423112102350159872'};
 const colors = ['ff2828','ff3d28','ff4b28','ff5a28','ff6828','ff7628','ff8c28','ffa128','ffac28','ffb728','ffc228','ffd028','ffd728','ffe228','fff028','fffb28','edff28','deff28','d0ff28','c2ff28','b3ff28','9aff28','8cff28','7dff28','6fff28','5aff28','3dff28','28ff2b','28ff41','28ff56','28ff6c','28ff81','28ff93','28ffa9','28ffba','28ffc9','28ffde','28fff4','28ffff','28f0ff','28deff','28deff','28d3ff','28c5ff','28baff','28b0ff','28a5ff','289eff','2893ff','2885ff','2876ff','2864ff','2856ff','284bff','2841ff','2836ff','2828ff','3228ff','4428ff','5328ff','6828ff','7628ff','7e28ff','8828ff','9328ff','a128ff','b028ff','be28ff','c928ff','d328ff','db28ff','e528ff','f028ff','ff28ff','ff28f7','ff28e5','ff28de','ff28d0','ff28c9','ff28ba','ff28b3','ff28a5','ff289a','ff288c','ff2881','ff287a','ff2873','ff2868','ff2861','ff2856','ff284f','ff2848','ff2844','ff282b'];
 
 client.on("ready", () => {
@@ -109,7 +109,8 @@ client.on('message', async (message) => {
         }
         if (message.channel.guild.region == "russia") {
             message.channel.guild.region = "Россия"
-        }
+        } 
+        const emoj = client.emojis.get(emojis.loading..); message.channel.send(`${emoj}`);
         
                 const embed = new Discord.RichEmbed()
                 .setTitle('Информация об сервере', message.channel.guild.name)
@@ -128,6 +129,7 @@ client.on('message', async (message) => {
                 .setTimestamp(); message.react("✅");
             message.channel.send({embed});
     } else if(['h', 'help'].includes(command)) {
+        const emoj = client.emojis.get(emojis.loading..); message.channel.send(`${emoj}`);
         const embed = new Discord.RichEmbed()
             .setTitle('Команды бота.')
             .setColor("#42f4aa")
@@ -137,6 +139,7 @@ client.on('message', async (message) => {
             .setTimestamp();
         message.channel.send({embed});
     } else if(['userinfo', 'ui'].includes(command)) {
+        const emoj = client.emojis.get(emojis.loading..); message.channel.send(`${emoj}`);
                 message.delete().catch(O_o => {});
         let member = message.guild.members.get(message.author.id);
 
@@ -190,6 +193,7 @@ client.on('message', async (message) => {
         message.guild.channels.get(chat).send(sayMessage).catch(()=>{message.reply('ты ебобо?');});
         message.delete().catch(O_o=>{});
     } else if(['invite'].includes(command)) {
+        const emoj = client.emojis.get(emojis.loading..); message.channel.send(`${emoj}`);
         const embed = new Discord.RichEmbed()
             .setTitle('Приглашение бота на ваш сервер.')
             .setColor("#0000ff")
