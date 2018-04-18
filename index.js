d97cad4392993b5bd972739274cf6d0c52c747f4
Артем Кучуров <35427055+X-495@users.noreply.github.com>
2018-04-18 15:41:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -162,6 +162,7 @@ client.on('message', async (message) => {
         let member = message.mentions.members.first();
     args.shift();
     const WarnMessage = args.join(" ");
+        if (user.user.id === message.author.id) return message.channel.send("Невозможно выписать предупреждение самому себе.")
     message.channel.send(`Пользователь ${member.user} получил предупреждение по причине **` + WarnMessage + "**");
     } else if (['embedsay'].includes(command)) {
         const embedsayMessage = args.join(" ");
