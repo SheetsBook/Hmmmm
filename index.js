4e19c9ae0b73daca58bda646a6901bd663fccfec
Артем Кучуров <35427055+X-BueStar@users.noreply.github.com>
2018-05-13 00:00:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -263,7 +263,24 @@ message.delete();
             message.react("❎")
             //Ставит реакцию (Несогласен).
         }).catch(function() {});
-    } else if (['avatar', 'av'].includes(command)) {
+    } else if (['kick'].includes(command)) {
+            const user = message.mentions.users.first();
+    if (user) {
+      const member = message.guild.member(user);
+      if (member) {
+        member.kick('Optional reason that will display in the audit logs').then(() => {
+          message.reply(`Successfully kicked ${user.tag}`);
+        }).catch(err => {
+          message.reply('I was unable to kick the member');
+          console.error(err);
+        });
+      } else {
+        message.reply('That user isn\'t in this guild!');
+      }
+    } else {
+      message.reply('You didn\'t mention the user to kick!');
+    }
+  } else if (['avatar', 'av'].includes(command)) {
         //задает 1 слово как пользователя
         let member = message.mentions.members.first();
         //если пользователь не найден или вписано не правильно выдает ошибку
