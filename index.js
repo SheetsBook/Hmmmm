92b005a507d86068fa912752c97ead7998d66bfb
Артем Кучуров <35427055+X-BueStar@users.noreply.github.com>
2018-05-15 20:25:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -112,11 +112,6 @@ message.delete();
         let sms = (args[1])
         let reaction = (args[2])
          client.channels.get(kanal).fetchMessage(sms).then(msg => {msg.react(reaction)});                                                                     
-    } else if(['idreact' ,'ir'].includes(command)) {
-        let kanal = (args[0])
-        let sms = (args[1])
-        let reaction = (args[2])
-     client.channels.get(kanal).fetchMessage(sms).then(msg => {msg.react(client.emojis.get(reaction))
     } else if(['choose'].includes(command)) {
         
         if(!args[1]) return message.channel.send("**Слишком мало выборов, Пример: да нет**");
