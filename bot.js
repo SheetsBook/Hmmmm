8f01cbcb44894e4a1b7c29a5b3e3552ec73f7bab
Артем Кучуров <35427055+X-BueStar@users.noreply.github.com>
2018-05-26 14:28:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -435,7 +435,7 @@ client.on("guildDelete", guild => {
         const deleteCount = parseInt(args[0], 10);
     
     if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+      return message.reply("Введите от 2 до 99");
     
     const fetched = await message.channel.fetchMessages({count: deleteCount});
     message.channel.bulkDelete(fetched)
