62ca5e3e1bec39cb72a4188da06a6296fe8cc307
Артем Кучуров <35427055+X-BueStar@users.noreply.github.com>
2018-06-03 00:46:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ vm.createContext(codeContext);
 //Клиент бота
 const client = new Discord.Client();
 //префикс
-const prefix = " ";
+const prefix = "x!";
 //массив ролей
 const rule = {own: "421373056158662656", admin: "421244086557605888"};
 //массив сосздателей
